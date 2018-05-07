9d25c026ec066cb8505bb4eb5637cf33240b4d4f
remrobem <rob.martin.5758@gmail.com>
2018-05-07 19:44:08 -0400

    initial

--- server02.js
+++ server02.js
@@ -2,25 +2,39 @@
 var http = require("http");
 
 // Define a port to listen for incoming requests
-var PORT = 7000;
+var PORT0 = 7000;
+var PORT1 = 7001;
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest0(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end('If you can read this, then you are still alive. Good for you.');
+  response.end('00 If you can read this, then you are still alive. Good for you.');
 }
+function handleRequest1(request, response) {
 
+    // Send the below string to the client when the user visits the PORT URL
+    response.end('01 If you can read this, then you are still alive. Good for you.');
+  }
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server0 = http.createServer(handleRequest0);
+var server1 = http.createServer(handleRequest1);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+server0.listen(PORT0, function() {
 
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT0);
 });
+server1.listen(PORT1, function() {
+
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT1);
+  });
+
+
+
 
 
 // port 7500 */
